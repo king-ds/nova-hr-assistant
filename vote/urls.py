9dd34da7f82ddd19c82bacc5dfd1a8dd22f9d75c
benjcabalona1029 <benjamin.cabalonajr@novare.com.hk>
2019-09-02 18:32:33 +0800

    Vote and comment feature

--- vote/urls.py
+++ vote/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('vote/new/', views.vote_page, name='vote_page'),
     path('vote/submit/', views.submit_vote, name='submit_vote'),
-    path('post/new/', views.create_post, name='add_post')
+
 ]
