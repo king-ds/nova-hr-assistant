f2c01078f489eef350617257d96c98b975df5d77
JustinRamirez <justin.ramirez@gmail.com>
2019-09-02 11:20:21 +0800

    app names

---
+++ gmail_authentication/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class GmailAuthenticationConfig(AppConfig):
+    name = 'gmail_authentication'
---
+++ vote_feed/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class VoteFeedConfig(AppConfig):
+    name = 'vote_feed'
