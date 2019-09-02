590fcaed54b313c0c551d81ee3e38ef659cb5291
Ricardo Calura <ricardocalura94@gmail.com>
2019-09-02 14:52:22 +0800

    Modified logout_user URL

--- gmail_authentication/urls.py
+++ gmail_authentication/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('welcome', views.welcome_page, name='welcome_page'),
-    path('logout/', views.logout_user, name='logout'),
+    path('logout_user/', views.logout_user, name='logout_user'),
     path('configure-account/', views.configure_account, name='configure_account'),
     path('', views.home, name='home'),
 ]
