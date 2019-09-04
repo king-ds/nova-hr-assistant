d6b69fd6f2a0612fd05de3fa205c2c9b89202b7b
benjcabalona1029 <benjamin.cabalonajr@novare.com.hk>
2019-09-04 14:19:06 +0800

    Added department & title to user's profile

--- profile_feed/views.py
+++ profile_feed/views.py
@@ -1,27 +1,27 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from gmail_authentication.models import User
+from pypiper.scraper import DataScrape
 
 @login_required(login_url='welcome_page')
 def profile(request, username):
-	# Identify user
-	user = User.objects.get(username=request.user)
-	# Get the top 5 most liked post
-	# Top_Posts = Post.objects.all().order_by("-num_likes")[:5]
 	# Get user instance
-	User_Instance = User.objects.get(username=request.user)
-	# Get user posts
-	# User_Posts = Post.objects.all().filter(username=User_Instance)
+	user_instance = User.objects.get(username=request.user)
+	scrape = DataScrape(user_instance.email)
+	profile_picture = scrape.get_profile_picture()
+	title = scrape.get_title()
+	department = scrape.get_department()
+
 	# Rendered context
 	context = {
-		'first_name' : user.first_name,
-		'last_name' : user.last_name,
-		'username' : user.username,
-		'email' : user.email,
-		'date_joined' : user.datetime_joined,
-        # 'Top_Posts' : Top_Posts,
-        # 'User_Posts' : User_Posts,
-
+		'first_name' : user_instance.first_name,
+		'last_name' : user_instance.last_name,
+		'username' : user_instance.username,
+		'email' : user_instance.email,
+		'date_joined' : user_instance.datetime_joined,
+		'profile_picture': profile_picture,
+		'title' : title,
+		'department' : department,
 	}
 	return render(request, 'profile_feed/profile.html', context)
 
